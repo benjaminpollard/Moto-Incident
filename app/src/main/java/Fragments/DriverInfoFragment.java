bcd70aec8f663b801ab9c15f04e5654bea638fab
benjaminpollard <benpollard1990@gmail.com>
2016-11-22 22:46:12 +0000

    more work

--- app/src/main/java/Fragments/DriverInfoFragment.java
+++ app/src/main/java/Fragments/DriverInfoFragment.java
@@ -97,12 +97,12 @@ public class DriverInfoFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
     }
 
     @Override
