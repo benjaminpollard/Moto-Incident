e5c1dce730beaf2754775f4d35e45f0335c14f4c
benjaminpollard <benpollard1990@gmail.com>
2016-11-29 23:09:06 +0000

    remove need for listener , todo replace/ put back in

--- app/src/main/java/Fragments/DescriptionFragment.java
+++ app/src/main/java/Fragments/DescriptionFragment.java
@@ -71,12 +71,7 @@ public class DescriptionFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+
     }
 
     @Override
