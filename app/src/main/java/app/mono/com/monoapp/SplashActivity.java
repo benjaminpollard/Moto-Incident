a6a0bf5661089869636ea7e4484955715feeaa04
benjaminpollard <benpollard1990@gmail.com>
2017-01-05 21:22:11 +0000

    init for realm

--- app/src/main/java/app/mono/com/monoapp/SplashActivity.java
+++ app/src/main/java/app/mono/com/monoapp/SplashActivity.java
@@ -8,12 +8,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import io.realm.Realm;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Realm.init(this);
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
