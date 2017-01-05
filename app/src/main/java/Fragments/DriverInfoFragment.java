613149e3ef70a9c61cc07104dd2dea7fcd71a724
benjaminpollard <benpollard1990@gmail.com>
2017-01-05 12:05:08 +0000

    fix bug and auto gen code clean up

--- app/src/main/java/Fragments/DriverInfoFragment.java
+++ app/src/main/java/Fragments/DriverInfoFragment.java
@@ -44,31 +44,16 @@ public class DriverInfoFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DriverInfoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DriverInfoFragment newInstance(String param1, String param2) {
+
+    public static DriverInfoFragment newInstance() {
         DriverInfoFragment fragment = new DriverInfoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -78,7 +63,7 @@ public class DriverInfoFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_driver_info, container, false);
         ButterKnife.bind(this,view);
 
-        ToolBarHelper.SetUpFragmentToolBar(this,toolbar,getActivity().getString(R.string.description_fragment_name));
+        ToolBarHelper.SetUpFragmentToolBar(this,toolbar,getActivity().getString(R.string.driver_informacion_fragment_name));
 
         return view;
     }
--- app/src/main/java/Fragments/LocationFragment.java
+++ app/src/main/java/Fragments/LocationFragment.java
@@ -29,14 +29,6 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LocationFragment extends Fragment implements OnMapReadyCallback {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private LocationModel locationModel;
     private OnLocationFragmentSubmitted mListener;
@@ -50,40 +42,33 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
     public LocationFragment() {
         // Required empty public constructor
     }
-
-
-    // TODO: Rename and change types and number of parameters
-    public static LocationFragment newInstance(String param1, String param2) {
+    
+    public static LocationFragment newInstance() {
         LocationFragment fragment = new LocationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_location, container, false);
+
         ButterKnife.bind(this, view);
+
+        ToolBarHelper.SetUpFragmentToolBar(this,toolbar,getActivity().getString(R.string.location_fragment_name));
 
         mapView.onCreate(savedInstanceState);
 
         mapView.getMapAsync(this);
 
-        ToolBarHelper.SetUpFragmentToolBar(this,toolbar,getActivity().getString(R.string.description_fragment_name));
 
-        return inflater.inflate(R.layout.fragment_location, container, false);
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
